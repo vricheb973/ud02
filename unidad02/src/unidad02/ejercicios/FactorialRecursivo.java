ee8c0e1d7c5f4becf99e280787ad718d9a80dd5d
vricheb973 <vricheb973@g.educaand.es>
2025-12-09 14:46:08 +0100

    Update FactorialRecursivo.java

--- unidad02/src/unidad02/ejercicios/FactorialRecursivo.java
+++ unidad02/src/unidad02/ejercicios/FactorialRecursivo.java
@@ -5,11 +5,8 @@ import java.math.BigInteger;
 public class FactorialRecursivo {
 
 	public static void main(String[] args) {
-		
-		// 5! = 5 x 4 x 3 x 2 x 1
-		
 
-		System.out.println(fibonacci(BigInteger.valueOf(40)));
+		System.out.println(fibonacci(40));
 
 	}
 	
