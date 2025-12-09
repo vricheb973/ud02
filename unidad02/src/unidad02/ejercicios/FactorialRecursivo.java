f78434e044bf94cbacd19623bd8ec37cb7c9b2ca
vricheb973 <vricheb973@g.educaand.es>
2025-12-09 14:45:34 +0100

    Update FactorialRecursivo.java

--- unidad02/src/unidad02/ejercicios/FactorialRecursivo.java
+++ unidad02/src/unidad02/ejercicios/FactorialRecursivo.java
@@ -1,22 +1,54 @@
 package unidad02.ejercicios;
 
+import java.math.BigInteger;
+
 public class FactorialRecursivo {
 
 	public static void main(String[] args) {
+		
+		// 5! = 5 x 4 x 3 x 2 x 1
+		
 
-		System.out.println(factorial(5));
+		System.out.println(fibonacci(BigInteger.valueOf(40)));
 
 	}
 	
-	public static int factorial(int n) {
-		if(n == 2) {
-			return 2;
+	public static int factorialIterativo(int n) {
+		int factorial = 1; 
+		
+		for(int i=n; i>=2; i--) {
+			factorial = factorial * i;
 		}
-		if(n == 1) {
+		
+		return factorial;
+	}
+	
+	public static int factorial(int n) {
+		if(n == 0) {
 			return 1;
 		}
 		
 		return n * factorial(n-1);
 	}
+	
+	public static int fibonacci(int n) {
+		if(n == 1) {
+			return 1;
+		}	
+		if(n == 2) {
+			return 1;
+		}
+		
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
 
+	public static int division(int dividendo, int divisor) {
+		if(dividendo < divisor) {
+			return 1;
+		}
+		
+		return 1 + division(dividendo-divisor, divisor);
+	}
+	
+	
 }
